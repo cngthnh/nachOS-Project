97a923e70caf784cfead94a4f99bafea40a2a8f6
ngotruonghuy <truonghuycorn@gmail.com>
2021-10-06 15:49:18 +0700

    - Add help.c

---
+++ nachos/NachOS-4.0/code/test/help.c
@@ -0,0 +1,17 @@
+#include "syscall.h"
+int main()
+{
+
+	PrintString("- Nhom gom 3 thanh vien: \n");
+	PrintString("> 19120183 - Pham Ba Cuong\n");
+	PrintString("> 19120242 - Ngo Truong Huy\n");
+	PrintString("> 19120374 - Vu Cong Thanh\n");
+	PrintString("\n");
+	PrintString("- Chuong trinh ascii dung de in ra cac ky tu doc duoc cua bang ma ASCII.\n");
+	PrintString("\n");
+	PrintString("- Chuong trinh sort dung de sap xep mang theo thu tu tang dan hoac\n");
+	PrintString("giam dan tuy theo yeu cau cua nguoi dung. Thuan toan duoc dung la\n");
+	PrintString("bubble sort. Nguoi dung su dung bang cach nhap n (so phan tu) roi\n");
+	PrintString("lan luot nhap vao cac phan tu. Chuong trinh se in ra mang da sap xep.");
+	return 0;
+}
